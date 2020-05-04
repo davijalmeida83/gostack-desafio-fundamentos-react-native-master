8ec0edd06de8987e09eb2cb067745ce72347cf5e
Davi Almeida <davij_almeida@hotmail.com>
2020-05-04 18:23:30 -0300

    Initial commit

---
+++ src/components/FloatingCart/index.tsx
@@ -0,0 +1,56 @@
+import React, { useState, useMemo } from 'react';
+
+import { useNavigation } from '@react-navigation/native';
+
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import {
+  Container,
+  CartPricing,
+  CartButton,
+  CartButtonText,
+  CartTotalPrice,
+} from './styles';
+
+import formatValue from '../../utils/formatValue';
+
+import { useCart } from '../../hooks/cart';
+
+// Calculo do total
+// Navegação no clique do TouchableHighlight
+
+const FloatingCart: React.FC = () => {
+  const { products } = useCart();
+
+  const navigation = useNavigation();
+
+  const cartTotal = useMemo(() => {
+    const sum = products.reduce(
+      (total, { price, quantity }) => total + price * quantity,
+      0,
+    );
+    return formatValue(sum);
+  }, [products]);
+
+  const totalItensInCart = useMemo(() => {
+    const sum = products.reduce((total, { quantity }) => total + quantity, 0);
+    return sum;
+  }, [products]);
+
+  return (
+    <Container>
+      <CartButton
+        testID="navigate-to-cart-button"
+        onPress={() => navigation.navigate('Cart')}
+      >
+        <FeatherIcon name="shopping-cart" size={24} color="#fff" />
+        <CartButtonText>{`${totalItensInCart} itens`}</CartButtonText>
+      </CartButton>
+
+      <CartPricing>
+        <CartTotalPrice>{cartTotal}</CartTotalPrice>
+      </CartPricing>
+    </Container>
+  );
+};
+
+export default FloatingCart;
---
+++ src/hooks/cart.tsx
@@ -0,0 +1,114 @@
+import React, {
+  createContext,
+  useState,
+  useCallback,
+  useContext,
+  useEffect,
+} from 'react';
+
+import AsyncStorage from '@react-native-community/async-storage';
+
+interface Product {
+  id: string;
+  title: string;
+  image_url: string;
+  price: number;
+  quantity: number;
+}
+
+interface CartContext {
+  products: Product[];
+  addToCart(item: Product): void;
+  increment(id: string): void;
+  decrement(id: string): void;
+}
+
+const CartContext = createContext<CartContext | null>(null);
+
+const CartProvider: React.FC = ({ children }) => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    async function loadProducts(): Promise<void> {
+      const cart = await AsyncStorage.getItem('@cart');
+      if (cart) {
+        setProducts(JSON.parse(cart));
+      }
+    }
+
+    loadProducts();
+  }, []);
+
+  const addToCart = useCallback(
+    async product => {
+      let cart: Product[] = [];
+
+      if (products.find(p => p.id === product.id)) {
+        cart = products.map(p => {
+          if (p.id === product.id) {
+            p.quantity += 1;
+          }
+          return p;
+        });
+      } else {
+        cart = [...products, { ...product, quantity: 1 }];
+      }
+
+      setProducts(cart);
+      await AsyncStorage.setItem('@cart', JSON.stringify(products));
+    },
+    [products],
+  );
+
+  const increment = useCallback(
+    async id => {
+      const cart = products.map(p => {
+        if (p.id === id) {
+          p.quantity += 1;
+        }
+        return p;
+      });
+
+      setProducts(cart);
+      await AsyncStorage.setItem('@cart', JSON.stringify(products));
+    },
+    [products],
+  );
+
+  const decrement = useCallback(
+    async id => {
+      const cart = products.map(p => {
+        if (p.id === id) {
+          p.quantity -= 1;
+        }
+        return p;
+      });
+
+      const filtered = cart.filter(item => item.quantity > 0);
+
+      setProducts(filtered);
+
+      await AsyncStorage.setItem('@cart', JSON.stringify(filtered));
+    },
+    [products],
+  );
+
+  const value = React.useMemo(
+    () => ({ addToCart, increment, decrement, products }),
+    [products, addToCart, increment, decrement],
+  );
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+function useCart(): CartContext {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw new Error(`useCart must be used within a CartProvider`);
+  }
+
+  return context;
+}
+
+export { CartProvider, useCart };
